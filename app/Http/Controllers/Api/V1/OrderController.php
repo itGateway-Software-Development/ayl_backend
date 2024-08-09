52e73f6313f5646a65178c006e2401824c1d2e8f
itGateway-Software-Development <developer@itgateway.org>
2024-08-09 22:11:57 +0630

    updte

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -77,9 +77,9 @@ class OrderController extends Controller
             ];
 
 
-            Mail::to('aylorder@gmail.com')->send(new OrderMail($mailData));
+            // Mail::to('aylorder@gmail.com')->send(new OrderMail($mailData));
             // Mail::to('lin911460@gmail.com')->send(new OrderMail($mailData));
-            // Mail::to('sawsantun147@gmail.com')->send(new OrderMail($mailData));
+            Mail::to('sawsantun147@gmail.com')->send(new OrderMail($mailData));
 
             if($user) {
                 $order = Order::where('user_id', $user->id)->orderBy('created_at', 'desc')->get();
